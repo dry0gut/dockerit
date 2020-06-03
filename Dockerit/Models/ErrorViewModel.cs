1a25368f7d89986f027ceea18643f4afee3ff96d
Edinson Morales <moralejov@gmail.com>
2020-06-02 21:48:49 -0500

    Creating base project for continuos integration curse

---
+++ Dockerit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dockerit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
